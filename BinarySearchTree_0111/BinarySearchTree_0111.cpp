e8fdea2ddc1cc4161d82cf811c5076a7648ecedd
syihwamoza <mozakastella26@gmail.com>
2024-06-14 10:26:54 +0700

    Performs the postorder traversal of the tree

--- BinarySearchTree_0111/BinarySearchTree_0111.cpp
+++ BinarySearchTree_0111/BinarySearchTree_0111.cpp
@@ -56,7 +56,54 @@ public:
 
     void search(string element, Node*& parent, Node*& currentNode)
     {
+        currentNode = ROOT;
+        parent = NULL;
+        while ((currentNode != NULL) && (currentNode->info) != element)
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
 
+    void inorder(Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is Empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
+
+    void preorder(Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree i Empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
+
+    void postorder(Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+
+        }
     }
 };
 
